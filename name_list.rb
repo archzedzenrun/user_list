442f66730d25b01292b146af7e01f26ad47b2d9e
archzedzenrun <archzedzenrun@gmail.com>
2023-06-30 16:08:34 -0700

    Adding files to repository.

---
+++ name_list.rb
@@ -0,0 +1,35 @@
+require "sinatra"
+require "sinatra/reloader"
+require "tilt/erubis"
+require "yaml"
+
+before do
+  @users = YAML.load_file("./data/users.yml").sort.to_h
+end
+
+helpers do
+  def count_users
+    @users.size
+  end
+
+  def count_interests
+    @users.map { |_, value| value[:interests].size }.sum
+  end
+end
+
+get "/" do
+  @title = "User list"
+  @use = params[:use]
+  erb :home
+end
+
+get "/users/:name" do
+  @name = params[:name]
+  @title = @name
+  @info = @users[@name.to_sym]
+  erb :user
+end
+
+not_found do
+  redirect "/"
+end
